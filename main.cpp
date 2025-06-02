06813fb86e83462ad5c068284c89bef72245b101
Walid Outtaleb <walidouttaleb@live.fr>
2025-06-02 19:29:18 +0200

    Call the exporter from main

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include "src/mesh.hpp"
 #include "src/scene.hpp"
 #include "src/utils.hpp"
+#include "src/exporter.hpp"
 
 
 int main() {
@@ -11,25 +12,28 @@ int main() {
     Eigen::Vector3d A, B, C;
     A << -1., 3., 1.;
     B << 1., 3., 1.;
-    C << 0.5, 3., -1.;
+    C << 5, 3., -1.;
 
     Triangle triangle(A, B, C);
 
 
-    Camera myCam(1.5, 1.5, 25, 25, 1.0);
+    Camera myCam(1.5, 1.5, 800, 800, 1.0);
     Scene myScene(&myCam);
     Render rdr = myScene.getRender();
 
-    std::cout << rdr.render << std::endl;
+    // std::cout << rdr.render << std::endl;
 
-    std::cout << "\n \nNow the testing" << std::endl;
+    // std::cout << "\n \nNow the testing" << std::endl;
 
     
 
     myScene.addTriangle(&triangle);
 
     rdr = myScene.getRender();
-    std::cout << rdr.render << std::endl;
+    // std::cout << rdr.render << std::endl;
+
+    auto exporter = MakeExporter(rdr.render);
+    exporter.toBitmap("render.bmp");
 
     return 0;
 }
